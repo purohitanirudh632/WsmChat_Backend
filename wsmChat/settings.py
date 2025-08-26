6fddf3b3a6bd576f07a24b00d69757f2d54a271a
Anirudh <purohitanirudh632@gmail.com>
2025-08-26 13:32:32 +0530

    dual databse  established

--- wsmChat/settings.py
+++ wsmChat/settings.py
@@ -88,16 +88,21 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'chatdb',
-        'USER': 'chatuser',
-        'PASSWORD':'1234',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'chatdb',
+    #     'USER': 'chatuser',
+    #     'PASSWORD':'1234',
+    #     'HOST':'localhost',
+    #     'PORT':'5432',
+    # }
+
+        'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    )
 }
 
 
